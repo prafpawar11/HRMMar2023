1a175d0ff72084291d1ceaf7a54fd4e569b7c34c
Suraj Patil <prafpawar11@gmail.com>
2023-03-25 19:48:48 +0530

    Demo1 code by Suraj

---
+++ src/test/java/TestLayer/Demo1.java
@@ -0,0 +1,16 @@
+package TestLayer;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		String a="java";
+		
+		for(int i=a.length()-1;i>=0;i--)
+		{
+			System.out.print(a.charAt(i));
+		}
+	
+	}
+
+}
